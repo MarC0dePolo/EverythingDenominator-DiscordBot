1ece70ba3b2c6309624b570e953d0b662416d3ae
MarC0dePolo <marco030103@gmail.com>
2023-04-26 19:02:01 +0200

    Code documentation

--- scripts/everythingPlotterAlphaV2.py
+++ scripts/everythingPlotterAlphaV2.py
@@ -5,21 +5,19 @@ from mpl_finance import candlestick_ohlc
 import pandas as pd
 import matplotlib.dates as mpl_dates
 import yfinance as yf
-import matplotlib.pyplot as plt
-from mpl_finance import candlestick_ohlc
-import pandas as pd
-import matplotlib.dates as mpl_dates
-import yfinance as yf
 
+# Define a Chart class for plotting candlestick charts
 class Chart:
     
     plt.style.use('ggplot')
+    
+    # Initialize the Chart object with tickers, period, and interval parameters
     def __init__(self, tickers, period, interval) -> None:
         self.tickers = tickers
         self.period = period
         self.interval = interval
-        
-        # Download data from yf api and configure for chart plotting        
+    
+    # Download historical OHLC data using yfinance package and return a DataFrame object
     def getDataFrame(self):
         self.data = yf.download(tickers=self.tickers, period=self.period, interval=self.interval)
         self.data = pd.DataFrame(self.data)
@@ -27,23 +25,23 @@ class Chart:
         data = self.data
         return data
     
+    # Return the value setup (tickers, period, interval) of the Chart object
     def getValueSetup(self):
         return self.tickers, self.period, self.interval
-
+    
+    # Plot a candlestick chart using mpl_finance package and a DataFrame object containing OHLC data
     def plotChart(self, dataFrame):
         ohlc = dataFrame.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
         ohlc['Date'] = pd.to_datetime(ohlc['Date'])
         ohlc['Date'] = ohlc['Date'].apply(mpl_dates.date2num)
         ohlc = ohlc.astype(float)
-
+        
         fig, ax = plt.subplots()
-        # plot width with a dict depending on interval scale
         candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
 
-        # Setting labels & titles
         ax.set_xlabel('Date')
         ax.set_ylabel('Price')
-        fig.suptitle(f'{self.__str__()}')
+        fig.suptitle(None)
 
         # Formatting Date
         date_format = mpl_dates.DateFormatter('%d-%m-%Y')
@@ -52,31 +50,26 @@ class Chart:
         fig.tight_layout()
         plt.show()
 
-    #def __str__(self) -> str:
-     #   return f"{self.tickers} chart {self.period}-timeframe {self.interval}-candles"
-    
-
-    
-
+# Define a subclass of the Chart class for plotting the value of Ethereum in Bitcoin terms
 class MyCounterValueChart(Chart):
+    # Initialize the MyCounterValueChart object with the value setup and DataFrame objects for Ethereum and Bitcoin
     def __init__(self, valueSetup, valueDataFrame, CvalueDataFrame):
         self.tickerValue, self.periodValue, self.intervalValue = valueSetup
         self.valueDataFrame = valueDataFrame
         self.CvalueDataFrame = CvalueDataFrame
-
+    
+    # Calculate the value of Ethereum in Bitcoin terms and return a DataFrame object
     def calculateDataFrame(self):
+        # Divide Ethereum OHLC data by corresponding Bitcoin OHLC data for each date
         myChart = pd.DataFrame(self.valueDataFrame.loc[:, ['Open', 'High', 'Low', 'Close']]/self.CvalueDataFrame.loc[:, ['Open', 'High', 'Low', 'Close']])
         dates = self.CvalueDataFrame.loc[:, ['Date']]
         myChart = pd.concat([dates, myChart], axis=1)
         return myChart
 
+# Instantiate Chart objects for Bitcoin and Ethereum
 btcusd = Chart('BTC-USD', '1mo', '1d')
 ethusd = Chart("ETH-USD", "1mo", "1d")
-#btcusd.plotChart(btcusd.getDataFrame())
-print(btcusd)
-print(ethusd)
 
+# Instantiate a MyCounterValueChart object for Ethereum priced in Bitcoin terms using the value setup and DataFrame objects for Bitcoin and Ethereum
 eth_btc = MyCounterValueChart(btcusd.getValueSetup(), ethusd.getDataFrame(), btcusd.getDataFrame())
 eth_btc.plotChart(eth_btc.calculateDataFrame())
-#btcusd.plotChart()
-#eth_btc.plotChart(eth_btc.calculate())
